31e02830c91b91e32aaf0f4561debd752c3653e4
troat9824 <troat9824@gmail.com>
2022-02-21 14:45:15 -0600

    beginning questions

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+var questions = [{
+    question: "Which of these tags will emphasize text?",
+    answerA: "<em>",
+    answerB: "<emphasize>",
+    answerC: "<strong>",
+    answerD: "<italicize>",
+    correctAnswer: "a",
+}, 
+{
+    question: "What does the '*' symbol do in CSS?",
+    answerA: "selects all bullet points",
+    answerB: "selects everything in the body",
+    answerC: "selects everything on the page",
+    answerD: "it doesn't do anything",
+    correctAnswer: "c",
+},
+
+
+
+
+
+
+]
