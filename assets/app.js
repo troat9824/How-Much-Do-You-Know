52cc66b80ec35978a5330b6cfc7fc734be85aa36
troat9824 <troat9824@gmail.com>
2022-02-23 07:12:06 -0600

    working on questions

--- assets/app.js
+++ assets/app.js
@@ -16,83 +16,9 @@ var clearButton = document.getElementById('clear');
 var viewButton = document.getElementById('scores');
 
 // 
-
-// score
-const score = "";
+var quizQuestion = document.getElementById('quizQuestion')
 
 
-// timer
-
-
-
-function reset() {
-    
-};
-
-// page starts with just coding quiz challenge 'first-page', hiding all others
-// can choose to go straight to the high scores on nav bar
-function beginQuiz() {
-    beginning.style.display = "block";
-    questionsPage.style.display = "none";
-    ending.style.display = "none";
-    highScorePage.style.display = "none";
-    beginButton.addEventListener('click', startQuestions);
-};
-
-
-// begins the test, showing only 'questions' class, hiding all others
-function startQuestions() {
-    beginning.style.display = "none";
-    questionsPage.style.display = "block";
-    ending.style.display = "none";
-    highScorePage.style.display = "none";
-    
-    
-    // start over with 0 score and timer
-    const score = 0;
-    var timeLeft = 90;
-    var countdownBegin = setInterval(function() {
-        if(timeLeft <=0) {
-            clearInterval(countdownBegin);
-            testEnds();
-        } else {
-            document.getElementById('timer').innerHTML = "Timer: " + timeLeft;
-        }
-        timeLeft -= 1;
-    }, 1000);
-    // start questions
-
-
-    // answer 
-
-
-
-    // once finished with all of questions, function testEnds
-};
-
-
-// test ends, showing only 'last-page' class, hiding all others
-function testEnds() {
-    beginning.style.display = "none";
-    questionsPage.style.display = "none";
-    ending.style.display = "block";
-    highScorePage.style.display = "none";
-
-
-    submitScore.addEventListener('click', HighScores);
-};
-
-
-// after submitting initials, shows only 'high-scores' page, hiding all others
-function HighScores() {
-    beginning.style.display = "none";
-    questionsPage.style.display = "none";
-    ending.style.display = "none";
-    highScorePage.style.display = "block";
-
-    returnButton.addEventListener('click', beginQuiz);
-    clearButton.addEventListener('click', reset);
-};
 
 
 // questions
@@ -177,6 +103,96 @@ const questions = [{
     correctAnswer: "",
 },
 ];
+
+// score
+const score = "";
+const questionIndex = 0;
+const finalQuestion = questions.length;
+
+function reset() {
+    score = 0;
+    questionIndex = 0;
+
+    // clear local storage
+};
+
+// page starts with just coding quiz challenge 'first-page', hiding all others
+// can choose to go straight to the high scores on nav bar
+function beginQuiz() {
+    beginning.style.display = "block";
+    questionsPage.style.display = "none";
+    ending.style.display = "none";
+    highScorePage.style.display = "none";
+    beginButton.addEventListener('click', startQuestions);
+};
+
+
+// begins the test, showing only 'questions' class, hiding all others
+function startQuestions() {
+    beginning.style.display = "none";
+    questionsPage.style.display = "block";
+    ending.style.display = "none";
+    highScorePage.style.display = "none";
+    
+    
+    // start over with 0 score and timer
+    const score = 0;
+    const questionIndex = 0;
+    var timeLeft = 90;
+    var countdownBegin = setInterval(function() {
+        if(timeLeft <=0) {
+            clearInterval(countdownBegin);
+            testEnds();
+        } else {
+            document.getElementById('timer').innerHTML = "Timer: " + timeLeft;
+        }
+        timeLeft -= 1;
+    }, 1000);
+    // start questions, starts at 0 index
+    var qq = questions[questionIndex];
+
+    //  puts questions and answers on h1 and buttons
+    quizQuestion.innerHTML = qq.question;
+    buttonAnswerA.innerHTML = qq.answerA;
+    buttonAnswerB.innerHTML = qq.answerB;
+    buttonAnswerC.innerHTML = qq.answerC;
+    buttonAnswerD.innerHTML = qq.answerD;
+    
+    // button.addEventListener('click', checkAnswer);
+
+    // answer 
+
+
+
+    // once finished with all of questions, function testEnds
+};
+
+
+// test ends, showing only 'last-page' class, hiding all others
+function testEnds() {
+    beginning.style.display = "none";
+    questionsPage.style.display = "none";
+    ending.style.display = "block";
+    highScorePage.style.display = "none";
+
+
+    submitScore.addEventListener('click', HighScores);
+};
+
+
+// after submitting initials, shows only 'high-scores' page, hiding all others
+function HighScores() {
+    beginning.style.display = "none";
+    questionsPage.style.display = "none";
+    ending.style.display = "none";
+    highScorePage.style.display = "block";
+
+    returnButton.addEventListener('click', beginQuiz);
+    clearButton.addEventListener('click', reset);
+};
+
+
+
 
 // start on first page
 beginQuiz();
