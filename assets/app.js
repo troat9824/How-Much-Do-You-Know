414a6488261a779b6b16eec2813301fd6c4095c0
troat9824 <troat9824@gmail.com>
2022-02-23 06:39:00 -0600

    adding timer

--- assets/app.js
+++ assets/app.js
@@ -10,11 +10,24 @@ var buttonAnswerA = document.getElementById('a');
 var buttonAnswerB = document.getElementById('b');
 var buttonAnswerC = document.getElementById('c');
 var buttonAnswerD = document.getElementById('d');
+var submitScore = document.getElementById('submit');
+var returnButton = document.getElementById('return');
+var clearButton = document.getElementById('clear');
+var viewButton = document.getElementById('scores');
+
+// 
 
 // score
 const score = "";
 
 
+// timer
+
+
+
+function reset() {
+    
+};
 
 // page starts with just coding quiz challenge 'first-page', hiding all others
 // can choose to go straight to the high scores on nav bar
@@ -23,20 +36,38 @@ function beginQuiz() {
     questionsPage.style.display = "none";
     ending.style.display = "none";
     highScorePage.style.display = "none";
-    beginButton.addEventListener('click', nextQuestion);
+    beginButton.addEventListener('click', startQuestions);
 };
 
 
 // begins the test, showing only 'questions' class, hiding all others
-function nextQuestion() {
+function startQuestions() {
     beginning.style.display = "none";
     questionsPage.style.display = "block";
     ending.style.display = "none";
     highScorePage.style.display = "none";
+    
+    
     // start over with 0 score and timer
     const score = 0;
-
+    var timeLeft = 90;
+    var countdownBegin = setInterval(function() {
+        if(timeLeft <=0) {
+            clearInterval(countdownBegin);
+            testEnds();
+        } else {
+            document.getElementById('timer').innerHTML = "Timer: " + timeLeft;
+        }
+        timeLeft -= 1;
+    }, 1000);
     // start questions
+
+
+    // answer 
+
+
+
+    // once finished with all of questions, function testEnds
 };
 
 
@@ -44,7 +75,11 @@ function nextQuestion() {
 function testEnds() {
     beginning.style.display = "none";
     questionsPage.style.display = "none";
+    ending.style.display = "block";
     highScorePage.style.display = "none";
+
+
+    submitScore.addEventListener('click', HighScores);
 };
 
 
@@ -53,6 +88,10 @@ function HighScores() {
     beginning.style.display = "none";
     questionsPage.style.display = "none";
     ending.style.display = "none";
+    highScorePage.style.display = "block";
+
+    returnButton.addEventListener('click', beginQuiz);
+    clearButton.addEventListener('click', reset);
 };
 
 
@@ -139,4 +178,7 @@ const questions = [{
 },
 ];
 
+// start on first page
 beginQuiz();
+
+viewButton.addEventListener('click', HighScores);
